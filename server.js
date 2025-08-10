60b489d327d145965111361f3cff4c231ca57fc1
Ganja Hatori <ganjahatori@gmail.com>
2025-08-10 22:54:35 +0530

    Deploy ready

--- server.js
+++ server.js
@@ -1,42 +1,33 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
-const path = require("path");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 let players = [];
 
 io.on("connection", (socket) => {
-  console.log("Player connected:", socket.id);
+    console.log("New player:", socket.id);
+    players.push({ id: socket.id, position: 0 });
 
-  // Add new player
-  players.push({ id: socket.id, position: 0 });
-
-  // Send all players to the new connection
-  socket.emit("init", players);
-
-  // Broadcast when someone moves
-  socket.on("playerMove", (data) => {
-    const player = players.find(p => p.id === socket.id);
-    if (player) {
-      player.position = data.position;
-    }
+    socket.emit("init", players);
     io.emit("updateBoard", players);
-  });
 
-  socket.on("disconnect", () => {
-    console.log("Player disconnected:", socket.id);
-    players = players.filter(p => p.id !== socket.id);
-    io.emit("updateBoard", players);
-  });
+    socket.on("playerMove", ({ position }) => {
+        const player = players.find(p => p.id === socket.id);
+        if (player) player.position = position;
+        io.emit("updateBoard", players);
+    });
+
+    socket.on("disconnect", () => {
+        players = players.filter(p => p.id !== socket.id);
+        io.emit("updateBoard", players);
+    });
 });
 
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
